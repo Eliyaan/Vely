be64245440038496f26c6c5f90941e8a30da563f
Linklancien <linklancien@gmail.com>
2024-05-13 03:02:29 +0200

    loop fix

--- Blocks/loop.v
+++ Blocks/loop.v
@@ -23,15 +23,19 @@ pub fn (loop Loop) show(ctx  gg.Context) {
 	mut x := loop.x
 	expend_height	:= (1 + loop.size)*blocks_height  + 2*attach_decal
 	ctx.draw_rect_filled(x, loop.y, expend_block_width, expend_height + blocks_height, gx.red)
+	y := loop.y + expend_height
 	x += expend_block_width
 	// Start
 	ctx.draw_rect_filled(x, loop.y, start_block_width, blocks_height, gx.red)
-	// End for end of the loop
-	ctx.draw_rect_filled(x, (loop.y + expend_height), 2*end_block_width, blocks_height, gx.red)
+	ctx.draw_rect_filled(x, y, start_block_width, blocks_height, gx.red)
+	
 	x += start_block_width
 	// Attach
 	ctx.draw_rect_filled(x, (loop.y + attach_decal), mid_block_width, blocks_height, gx.red)
+	ctx.draw_rect_filled(x, y, mid_block_width, (blocks_height + attach_decal), gx.red)
+
 	x += mid_block_width
 	// END
 	ctx.draw_rect_filled(x, loop.y, end_block_width, blocks_height, gx.red)
+	ctx.draw_rect_filled(x, y, end_block_width, blocks_height, gx.red)
 }
