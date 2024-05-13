29f3337a93fadaddfb899c5b6c69746d3b04c9cc
Linklancien <linklancien@gmail.com>
2024-05-13 02:51:11 +0200

    Match

--- Blocks/match.v
+++ Blocks/match.v
@@ -17,6 +17,36 @@ pub:	id		int
 		size		[]int
 }
 
-pub fn (ma Match) show() {
-	
+pub fn (ma Match) show(ctx  gg.Context) {
+	mut x := ma.x
+	mut expend_height	:= (1 + ma.size[0])*blocks_height  + 2*attach_decal
+	x += expend_block_width
+	// Start
+	ctx.draw_rect_filled(x, ma.y, start_block_width, blocks_height, gx.red)
+	x += start_block_width
+	// Attach
+	ctx.draw_rect_filled(x, ma.y, mid_block_width, (blocks_height + attach_decal), gx.red)
+	x += mid_block_width
+	// END
+	ctx.draw_rect_filled(x, ma.y, end_block_width, blocks_height, gx.red)
+
+	for size in ma.size[1..] {
+		x = ma.x
+		y := ma.y + expend_height
+		expend_height	+= (1 + size)*blocks_height  + 2*attach_decal
+		x += expend_block_width
+		// Start
+		ctx.draw_rect_filled(x, y, start_block_width, blocks_height, gx.red)
+		x += start_block_width
+		// Attach
+		ctx.draw_rect_filled(x, y, mid_block_width, (blocks_height + attach_decal), gx.red)
+		x += mid_block_width
+		// END
+		ctx.draw_rect_filled(x, y, end_block_width, blocks_height, gx.red)
+	}
+	// End for end of the ma
+	x = ma.x 
+	ctx.draw_rect_filled(x, ma.y, expend_block_width, expend_height + blocks_height, gx.red)
+	x += expend_block_width
+	ctx.draw_rect_filled(x, (ma.y + expend_height), 2*end_block_width, blocks_height, gx.red)
 }
--- main.v
+++ main.v
@@ -36,7 +36,8 @@ fn main() {
     app.list_blocks << blocks.Input_output{2, blocks.Variants.input, 250, 100, -1, -1, []}
     app.list_blocks << blocks.Condition{3, blocks.Variants.condition, 100, 200, -1, -1, 0, 0, 0, 0, 0}
     app.list_blocks << blocks.Function{4, blocks.Variants.function, 250, 200, 0, 0, [], [], []}
-    app.list_blocks << blocks.Loop{4, blocks.Variants.loop, 400, 100, 0, -1, -1, -1, 0, []}
+    app.list_blocks << blocks.Loop{5, blocks.Variants.loop, 400, 100, 0, -1, -1, -1, 0, []}
+    app.list_blocks << blocks.Match{6, blocks.Variants.@match, 400, 250, -1, -1, [], [], [3, 2]}
     app.ctx.run()
 }
 
