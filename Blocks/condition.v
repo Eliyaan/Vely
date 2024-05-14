beeec309961903d8183b578e938838fb9c117c87
Linklancien <linklancien@gmail.com>
2024-05-14 19:03:31 +0200

    match dans les conditions

--- Blocks/condition.v
+++ Blocks/condition.v
@@ -10,36 +10,52 @@ pub:
 	pub mut:
 		x		int
 		y		int
-
-		con	int
+		
+		input	int
 		output	int
-		inner_if	int
-		condition	int
-		size_if		int
-		inner_else int
-		size_else	int
+		inner	[]int
+		conditions	[]int
+		size		[]int
 }
 
 pub fn (con Condition) show(ctx  gg.Context) {
 	mut x := con.x
-	if_expend_height	:= (1 + con.size_if)*blocks_height  + 2*attach_decal
-	else_expend_height	:= if_expend_height + (1 + con.size_else)*blocks_height  + 2*attach_decal
-	ctx.draw_rect_filled(x, con.y, expend_block_width, else_expend_height + blocks_height, gx.red)
+	mut expend_height	:= (1 + con.size[0])*blocks_height  + 2*attach_decal
 	x += expend_block_width
 	// Start
 	ctx.draw_rect_filled(x, con.y, start_block_width, blocks_height, gx.red)
-	ctx.draw_rect_filled(x, con.y + if_expend_height, start_block_width, blocks_height, gx.red)
-	ctx.draw_rect_filled(x, con.y + else_expend_height, start_block_width, blocks_height, gx.red)
-
 	x += start_block_width
 	// Attach
 	ctx.draw_rect_filled(x, (con.y + attach_decal), mid_block_width, blocks_height, gx.red)
-	ctx.draw_rect_filled(x, (con.y + if_expend_height), mid_block_width, (blocks_height + attach_decal), gx.red)
-	ctx.draw_rect_filled(x, (con.y + else_expend_height), mid_block_width, (blocks_height + attach_decal), gx.red)
-
 	x += mid_block_width
 	// END
 	ctx.draw_rect_filled(x, con.y, end_block_width, blocks_height, gx.red)
-	ctx.draw_rect_filled(x, (con.y + if_expend_height), end_block_width, blocks_height, gx.red)
-	ctx.draw_rect_filled(x, (con.y + else_expend_height), end_block_width, blocks_height, gx.red)
+
+	for size in con.size[1..] {
+		x = con.x
+		y := con.y + expend_height
+		expend_height	+= (1 + size)*blocks_height  + 2*attach_decal
+		x += expend_block_width
+		// Start
+		ctx.draw_rect_filled(x, y, start_block_width, blocks_height, gx.red)
+		x += start_block_width
+		// Attach
+		ctx.draw_rect_filled(x, y, mid_block_width, (blocks_height + attach_decal), gx.red)
+		x += mid_block_width
+		// END
+		ctx.draw_rect_filled(x, y, end_block_width, blocks_height, gx.red)
+	}
+	// End for end of the con
+	x = con.x 
+	ctx.draw_rect_filled(x, con.y, expend_block_width, expend_height + blocks_height, gx.red)
+	x += expend_block_width
+	y := con.y + expend_height
+	// Start
+	ctx.draw_rect_filled(x, y, start_block_width, blocks_height, gx.red)
+	x += start_block_width
+	// Attach
+	ctx.draw_rect_filled(x, y, mid_block_width, (blocks_height + attach_decal), gx.red)
+	x += mid_block_width
+	// END
+	ctx.draw_rect_filled(x, y, end_block_width, blocks_height, gx.red)
 }
